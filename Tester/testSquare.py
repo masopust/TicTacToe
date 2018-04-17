7e5efd4c513da4d81d600931f23bb8ea6196a861
Jan Masopust <masopust.jan@gmail.com>
2018-04-17 18:08:43 +0200

    Add some tests

--- Tester/testSquare.py
+++ Tester/testSquare.py
@@ -3,11 +3,14 @@ try:
     from TicTacToe import square
 except ModuleNotFoundError:
     import square
+    import field as playingField
+    import endValidator
+    import clickManager
 
 
 class SquareTester(unittest.TestCase):
 
-    def test_correct_creation(self):
+    def test_incorrect_creation(self):
         input_x = "wrong input"
         input_y = 1
         input_padding = 30
@@ -15,7 +18,7 @@ class SquareTester(unittest.TestCase):
         self.assertRaises(ValueError, square.Square, input_y, input_x, input_padding)
         self.assertRaises(ValueError, square.Square, input_x, input_y, input_x)
 
-    def test_incorrect_creation(self):
+    def test_correct_creation(self):
         input_x = 1
         input_y = 1
         input_padding = 30
@@ -43,6 +46,20 @@ class SquareTester(unittest.TestCase):
         self.assertRaises(TypeError, sq.click_inside, input_x, input_y)
         self.assertRaises(TypeError, sq.click_inside, input_y, input_x)
 
+    def test_correct_click_creation(self):
+        input_field = playingField.Field()
+        input_validator = endValidator.EndValidator(input_field)
+        clickManager.ClickManager(input_field, input_validator)
+        self.assertTrue("Class creation successful!")
+
+    def test_incorrect_switch(self):
+        input_x = "wrong_input"
+        input_y = 1
+        input_field = playingField.Field()
+        input_validator = endValidator.EndValidator(input_field)
+        cm = clickManager.ClickManager(input_field, input_validator)
+        self.assertRaises(TypeError, cm.switch_turns, input_x, input_y)
+        self.assertRaises(TypeError, cm.switch_turns, input_y, input_x)
 
 if __name__ == "__main__":
     unittest.main()
